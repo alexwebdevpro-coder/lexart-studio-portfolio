145007656b225adb848c34a3c180d18b65f38744
Alex.ZIRNHELT <alexis.zirnhelt@gmail.com>
2026-03-30 19:51:18 +0200

    feat: page Home - typographie animée - nouveau logotype v2 - description mise à jour

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,28 @@
-import { Link } from 'react-router-dom'
-import logotype from '../assets/svg/lexart_studio_logotype.svg'
-import photo from '../assets/image/Photo-Home2.JPG'
-import '../styles/Home.css'
+import { Link } from "react-router-dom";
+import logotype from "../assets/svg/lexart_studio_logotype_v2.svg";
+import photo from "../assets/image/Photo-Home2.JPG";
+import "../styles/Home.css";
 
 function Home() {
   return (
     <section className="home container">
-
       <div className="text-center mb-5">
         <img src={logotype} alt="Lexart Studio" className="home-logotype" />
       </div>
 
       <div className="row align-items-center g-5">
-
         <div className="col-12 col-lg-6">
-          <h1 className="home-name fw-bold mb-2">Alexis ZIRNHELT</h1>
-          <p className="home-role mb-4">Développeur web fullstack</p>
+          <h1 className="home-name mb-2">
+            Alexis <span>ZIRNHELT</span>
+          </h1>
+          <div className="home-role-wrapper">
+            <p className="home-role">Développeur web fullstack</p>
+            <span className="home-role-cursor">_</span>
+          </div>
           <p className="home-description mb-5">
-            Je conçois et développe des applications web modernes. À travers mes projets,
-            j'explore différentes technologies pour créer des expériences web efficaces
-            et accessibles.
+            En reconversion vers le développement web, je construis des
+            interfaces qui allient <span>esthétique et performance</span>.
+            Chaque projet est une occasion d'apprendre et de progresser.
           </p>
           <div className="d-flex gap-3 flex-wrap">
             <Link to="/portfolio" className="home-btn-primary">
@@ -36,11 +39,9 @@ function Home() {
             <img src={photo} alt="Alexis ZIRNHELT" className="home-photo" />
           </div>
         </div>
-
       </div>
-
     </section>
-  )
+  );
 }
 
-export default Home
+export default Home;
